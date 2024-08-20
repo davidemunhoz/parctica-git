8e76a280c1c17b950ed33c6c7081f703bfda0a79
David <em06020@ues.edu.sv>
2024-08-19 22:55:16 -0600

    Agregando proyecto a repoditorio

---
+++ config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 22:06:20 2024
+
+@author: David
+"""
+
+# API de Exchange Rate API
+API_KEY="mi_primera_api"
---
+++ currency_converter.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 19 22:06:01 2024
+
+@author: David
+"""
+
+import requests
+
+class CurrencyConverter:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.url = f"https://v6.exchangerate-api.com/v6/{api_key}/latest/"
+
+    def get_exchange_rate(self, from_currency, to_currency):
+        response = requests.get(self.url + from_currency)
+        data = response.json()
+        if response.status_code == 200:
+            rates = data['conversion_rates']
+            return rates.get(to_currency, None)
+        else:
+            print(f"Error: {data['error-type']}")
+            return None
